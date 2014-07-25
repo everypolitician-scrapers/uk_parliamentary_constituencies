3c3b67b37839887b369dd4cab2a5784dd91cee06
Tony Bowden <tony@tmtm.com>
2014-07-25 15:19:49 +0300

    simple scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,17 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Fetch voting information from riigikogu.ee
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+
+@URL = 'http://en.wikipedia.org/wiki/List_of_United_Kingdom_Parliament_constituencies'
+
+doc = Nokogiri::HTML(open(@URL).read)
+data = doc.css("table.wikitable tr").drop(1).map do |tr|
+  cells = tr.css('td')
+  { constituency: cells[0].text, country: cells[-1].text }
+end
+
+ScraperWiki.save_sqlite([:constituency], data)
+
